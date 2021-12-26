e0666e9426b1ca0ab1044a45376a0458c1a56642
minghsu <leo1997515@gms.tku.edu.tw>
2021-12-26 15:34:55 +0800

    add App Page and styles

---
+++ src/pages/App/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './index.pcss';
+
+export default function App() {
+  return (
+    <div className={styles.app}>
+      Hello World!
+    </div>
+  )
+}
